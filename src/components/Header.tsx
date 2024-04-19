d41894d5e7eb2f9c7c51132cc61b3c13f79b9e40
Tibor Fazekas <tibcso0322@gmail.com>
2024-04-19 13:17:56 +0200

    'wip'

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,16 @@ import search from '../assets/search.svg';
 
 export default function Header() {
 
-	return <header className='w-full h-[20%]'>
-	<nav className='w-full h-[50%] flex justify-between select-none'>
-		<img className='m-10' src={logo} alt="logo" />
-		<div className='w-20 h-full flex justify-center'>
-			<img src={search} alt="search" />
-			<img src={book} alt="book" />
-		</div>
-	</nav>
-	<section></section>
+	return <header className='w-full h-[60%] border-b-2'>
+		<nav className='w-full h-[15%] flex justify-between select-none'>
+			<img className='box-content px-10 w-12' src={logo} alt="logo" />
+			<div className='px-10 box-content w-28 h-full flex justify-between'>
+				<img src={search} alt="search" />
+				<img src={book} alt="book" />
+			</div>
+		</nav>
+		<section className='h-[85%] bg-[#F5F5F5] relative' id='khan-section'>
+
+		</section>
   </header>
 }
