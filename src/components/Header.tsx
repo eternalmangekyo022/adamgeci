18566729ef34f5149223d3f3c80a9fca86213bb9
Tibor Fazekas <tibcso0322@gmail.com>
2024-04-23 12:52:32 +0200

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,13 @@ import search from '../assets/search.svg';
 export default function Header() {
 
 	return <header className='w-full h-[20%]'>
-	<nav className='w-full h-[50%] flex justify-between select-none'>
-		<img className='m-10' src={logo} alt="logo" />
-		<div className='w-20 h-full flex justify-center'>
-			<img src={search} alt="search" />
-			<img src={book} alt="book" />
-		</div>
-	</nav>
-	<section></section>
+		<nav className='w-full h-[50%] flex justify-between select-none'>
+			<img className='w-12 ml-10' src={logo} alt="logo" />
+			<div className='w-40 mr-10 h-full flex justify-evenly'>
+				<img src={search} alt="search" />
+				<img src={book} alt="book" />
+			</div>
+		</nav>
+		<section></section>
   </header>
 }
